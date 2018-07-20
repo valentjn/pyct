b59848e13ed4c601ad0b8998263f77b98aea0c71
Julian Valentin <julian@bsplines.org>
2018-07-20 14:42:20 +0200

    Add first version of test_ct and testfcns

---
+++ testfcns.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import numpy as np
+import sympy as sp
+
+
+
+functionStrings = {
+  "branin" : ("(x1 - 51/10*x0^2/(4*pi^2) + 5*x0/pi - 6)^2 + "
+              "10*(1-1/(8*pi))*cos(x0) + 10", np.array([[-5, 0], [10, 15]])),
+}
+
+
+
+def getFunction(functionType, d):
+  return getFunctionDerivative(functionType, d, np.zeros((d,)))
+
+def getFunctionDerivative(functionType, d, order):
+  variables = [sp.var("x{}".format(t)) for t in range(d)]
+  functionString, bounds = functionStrings[functionType]
+  functionExpression = sp.sympify(functionString)
+  innerDerivative = 1
+  
+  for t in range(d):
+    if order[t] > 0:
+      functionExpression = sp.diff(functionExpression, variables[t], order[t])
+      innerDerivative *= (bounds[1,t] - bounds[0,t])**order[t]
+  
+  lambdaFcn = sp.utilities.lambdify(variables, functionExpression)
+  resultFcn = (lambda X: innerDerivative * lambdaFcn(
+      *(bounds[0] + X * (bounds[1] - bounds[0])).T))
+  
+  return resultFcn
+
+def getFunctionFirstDerivatives(functionType, d):
+  orders = d * [list(range(2))]
+  orders = np.meshgrid(*orders, indexing="ij")
+  orders = np.column_stack([order.flatten() for order in orders])
+  orders = orders.tolist()
+  orders = [tuple(order) for order in orders if sum(order) > 0]
+  derivativeFcns = {order : getFunctionDerivative(functionType, d, order)
+                    for order in orders}
+  resultFcn = (lambda X: {order : derivativeFcns[order](X)
+                          for order in orders})
+  return resultFcn
+
+def getFunctionGradient(functionType, d):
+  derivativeFcns = [getFunctionDerivative(
+      functionType, d, np.eye(1, d, t, dtype=int).flatten())
+      for t in range(d)]
+  resultFcn = (lambda X: np.column_stack(
+      [derivativeFcn(X) for derivativeFcn in derivativeFcns]))
+  
+  return resultFcn
